3c48702ad0d571c341b5e1d954ed559c8598f616
Syorito Hatsuki <enderdron3@gmail.com>
2021-08-11 00:27:20 +0300

    init repo [0.0.1]

---
+++ src/main.cpp
@@ -0,0 +1,183 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+
+/*  Pin defines  */
+#define BTN_FIRE 9
+#define BTN_SUBTRACT 10
+#define BTN_ADD 11
+#define SEMISTOR 12
+
+/*  Serial defines  */
+#define SERIAL_SPEED 9600
+
+/*  LCD defines  */
+#define BOOT_DELAY_MS 2000
+#define LCD_TITLE "CBAPKA"
+#define LCD_SOURCE "cv.fstudio.dev"
+
+/*  repeat count  */
+unsigned char repeatCount = 5;
+
+/*  delay time in ms for repeat  */
+unsigned short int delayTimeInMs = 50;
+
+/*  Button pressed status  */
+bool isAddBtnPressed = false;
+bool isSubtractBtnPressed = false;
+bool isFireBtnPressed = false;
+
+bool updateDisplay = true;
+
+/*   Method init   */
+void initSystem();
+void initLCD();
+
+void draw(unsigned char pointer);
+void drawPointer(unsigned char pointer);
+
+void onAddPressed(unsigned char pointer);
+void onSubtractPressed(unsigned char pointer);
+unsigned char onAddAndSubtractHolded(unsigned char pointer);
+void onFirePressed();
+
+/*  Setup LCD pins  */
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+void setup()
+{
+  initSystem();
+}
+
+void initSystem()
+{
+  Serial.begin(SERIAL_SPEED);
+
+  pinMode(BTN_SUBTRACT, INPUT);
+  pinMode(BTN_ADD, INPUT);
+  pinMode(BTN_FIRE, INPUT);
+  pinMode(SEMISTOR, OUTPUT);
+
+  initLCD();
+}
+
+void initLCD()
+{
+  lcd.begin(12, 2);
+
+  lcd.setCursor(5, 0);
+  lcd.print(LCD_TITLE);
+  lcd.setCursor(1, 1);
+  lcd.print(LCD_SOURCE);
+  delay(BOOT_DELAY_MS);
+}
+
+void loop()
+{
+  static unsigned char pointer = 0;
+  draw(pointer);
+  onAddPressed(pointer);
+  onSubtractPressed(pointer);
+  pointer = onAddAndSubtractHolded(pointer);
+  onFirePressed();
+}
+
+void draw(unsigned char pointer)
+{
+  if (updateDisplay)
+  {
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("Repeat ");
+    lcd.setCursor(10, 0);
+    lcd.print(repeatCount);
+    lcd.setCursor(1, 1);
+    lcd.print("Delay ");
+    lcd.setCursor(10, 1);
+    lcd.print(delayTimeInMs);
+
+    drawPointer(pointer);
+
+    updateDisplay = false;
+  }
+}
+
+void drawPointer(unsigned char pointer)
+{
+  lcd.setCursor(9, pointer);
+  lcd.print("[");
+  lcd.setCursor(14, pointer);
+  lcd.print("]");
+}
+
+void onAddPressed(unsigned char pointer)
+{
+  if (digitalRead(BTN_ADD) && !isAddBtnPressed)
+  {
+    switch (pointer)
+    {
+    case 0:
+      if (repeatCount < 255) repeatCount++;
+      break;
+    case 1:
+      if (delayTimeInMs < 9999) delayTimeInMs++;
+      break;
+    }
+    isAddBtnPressed = true;
+    updateDisplay = true;
+  }
+  else if (!digitalRead(BTN_ADD) && isAddBtnPressed)
+  {
+    isAddBtnPressed = false;
+  }
+}
+
+void onSubtractPressed(unsigned char pointer)
+{
+  if (digitalRead(BTN_SUBTRACT) && !isSubtractBtnPressed)
+  { 
+    switch (pointer)
+    {
+    case 0:
+      if (repeatCount > 1) repeatCount--;
+      break;
+    case 1:
+      if (delayTimeInMs > 1) delayTimeInMs--;
+      break;
+    }
+    isSubtractBtnPressed = true;
+    updateDisplay = true;
+  }
+  else if (!digitalRead(BTN_SUBTRACT) && isSubtractBtnPressed)
+  {
+    isSubtractBtnPressed = false;
+  }
+}
+
+unsigned char onAddAndSubtractHolded(unsigned char pointer){
+  if (isAddBtnPressed && isSubtractBtnPressed)
+  {
+    return (pointer == 0) ? 1 : 0;
+  }
+  return pointer;
+}
+
+void onFirePressed()
+{
+  if (digitalRead(BTN_FIRE) && !isFireBtnPressed)
+  {
+    for (byte i = 1; i <= repeatCount; i++)
+    {
+      digitalWrite(SEMISTOR, HIGH);
+      delay(delayTimeInMs);
+      digitalWrite(SEMISTOR, LOW);
+      delay(delayTimeInMs);
+    }
+    isFireBtnPressed = true;
+    updateDisplay = true;
+  }
+  else if (!digitalRead(BTN_FIRE) && isFireBtnPressed)
+  {
+    isFireBtnPressed = false;
+  }
+}
